9a72a7af83403c28e401ad7e1730882b35aefcdb
OmarElnemr <omarahmedelnemr16@gmail.com>
2025-01-13 19:00:42 +0200

    Heroes Tech init

---
+++ assets/js/FAQ.js
@@ -0,0 +1,21 @@
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+  const question = item.querySelector('.faq-question');
+  const answer = item.querySelector('.faq-answer');
+  const arrow = item.querySelector('.arrow');
+
+  question.addEventListener('click', () => {
+    const isOpen = answer.classList.contains('open');
+
+    // Close all open answers
+    document.querySelectorAll('.faq-answer').forEach(a => a.classList.remove('open'));
+    document.querySelectorAll('.arrow').forEach(a => a.classList.remove('open'));
+
+    // Toggle current item
+    if (!isOpen) {
+      answer.classList.add('open');
+      arrow.classList.add('open');
+    }
+  });
+});
